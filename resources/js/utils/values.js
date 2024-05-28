c4a317dd27b45efee564ce67d19247bf94d57a34
Bede Abbe <bede.abbe@gmail.com>
2024-05-28 14:32:41 +0000

    url change

--- resources/js/utils/values.js
+++ resources/js/utils/values.js
@@ -1,7 +1,7 @@
 export const VALUES = {
     // GET CUSTOMER API
-    authenticationBaseApiEndpoint: `https://vmi1862596.contaboserver.net:8000`,
-    itemsBaseApiEndpoint: `https://vmi1862596.contaboserver.net:8002`,
-    customersBaseApiEndpoint: `https://vmi1862596.contaboserver.net:8001`
+    authenticationBaseApiEndpoint: `https://makufoodsltd.net:8000`,
+    itemsBaseApiEndpoint: `https://makufoodsltd.net:8002`,
+    customersBaseApiEndpoint: `https://makufoodsltd.net:8001`
     // baseApiEndpoint: `http://localhost:8082`
 }
