8b39ec63ab21d9b506f04fcd1054413a6c421f4d
Terminal-Adawe <bede.abbe91@gmail.com>
2026-01-15 22:38:49 +0100

    changes

--- resources/js/utils/values.js
+++ resources/js/utils/values.js
@@ -1,13 +1,13 @@
 export const VALUES = {
     // GET CUSTOMER API
-    // authenticationBaseApiEndpoint: `https://makufoodsltd.net:8000`,
-    // itemsBaseApiEndpoint: `https://makufoodsltd.net:8002`,
-    // customersBaseApiEndpoint: `https://makufoodsltd.net:8001`,
-    // transactionBaseApiEndpoint: `https://makufoodsltd.net:8005`,
-    // paymentBaseApiEndpoint: `https://makufoodsltd.net:8008`,
-    itemsBaseApiEndpoint: `http://localhost:4082`,
-    customersBaseApiEndpoint: `http://localhost:4083`,
-    authenticationBaseApiEndpoint: `http://localhost:4080`,
-    transactionBaseApiEndpoint: `http://localhost:4085`,
-    paymentBaseApiEndpoint: `http://localhost:4088`
+    authenticationBaseApiEndpoint: `https://makufoodsltd.net:4000`,
+    itemsBaseApiEndpoint: `https://makufoodsltd.net:4002`,
+    customersBaseApiEndpoint: `https://makufoodsltd.net:4001`,
+    transactionBaseApiEndpoint: `https://makufoodsltd.net:4005`,
+    paymentBaseApiEndpoint: `https://makufoodsltd.net:4008`,
+    // itemsBaseApiEndpoint: `http://localhost:4082`,
+    // customersBaseApiEndpoint: `http://localhost:4083`,
+    // authenticationBaseApiEndpoint: `http://localhost:4080`,
+    // transactionBaseApiEndpoint: `http://localhost:4085`,
+    // paymentBaseApiEndpoint: `http://localhost:4088`
 }
